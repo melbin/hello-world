424997af9f686b4e7727fddb6218960e4479c1f7
Melbin <melbin.cruz40@gmail.com>
2020-07-25 07:24:22 -0600

    testing jenkins 59

--- src/main/java/sv/edu/uesocc/AuthorationServerApplication.java
+++ src/main/java/sv/edu/uesocc/AuthorationServerApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 import io.jaegertracing.Configuration;
-import io.jaegertracing.Configuration.ReporterConfiguration;
-import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.opentracing.Tracer;
+
 
 //import brave.sampler.Sampler;
 
@@ -26,14 +25,26 @@ public class AuthorationServerApplication {
 //		return Sampler.ALWAYS_SAMPLE;
 //	}
 
-// For Kubernates solution
-
+	/*
+	 * No se si se usa unicamente en Docker (Probarlo, sino borrarlo)
+	 * Para Kubernates no sirve 
+	 * 	
 	@Bean
 	public io.opentracing.Tracer initTracer() {
 		SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
 		ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
 		return Configuration.fromEnv("hello-service").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
 	}
+	 */
+	
+// For Kubernates Only
+	
+	@Bean
+	  public Tracer initTracer() {
+	    Configuration.SamplerConfiguration samplerConfig = (new Configuration.SamplerConfiguration()).withType("const").withParam(Integer.valueOf(1));
+	    Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(Boolean.valueOf(true));
+	    return (Tracer)Configuration.fromEnv("hello-service").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorationServerApplication.class, args);
