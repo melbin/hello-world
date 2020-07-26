f8b9f1f893d7bfd28252ec4ffac37938169e6655
Melbin <melbin.cruz40@gmail.com>
2020-07-25 23:48:29 -0600

    testing jenkins 83

--- src/main/java/sv/edu/uesocc/controllers/HomeController.java
+++ src/main/java/sv/edu/uesocc/controllers/HomeController.java
@@ -48,7 +48,7 @@ public class HomeController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
 		logger.info("Test Method was called");
-		return "Congrats CI/CD Implemented Successfully!!!";
+		return "Congrats CI/CD Implemented Successfully!!! V1.1.7";
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
