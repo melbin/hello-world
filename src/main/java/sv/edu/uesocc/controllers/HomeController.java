368befc88a50db2baf3ce65006aca224e8a32259
Melbin <melbin.cruz40@gmail.com>
2020-08-02 18:14:19 -0600

    Changing hello-world version 1.2.7

--- src/main/java/sv/edu/uesocc/controllers/HomeController.java
+++ src/main/java/sv/edu/uesocc/controllers/HomeController.java
@@ -48,7 +48,7 @@ public class HomeController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
 		logger.info("Test Method was called");
-		return "Congrats CI/CD Implemented Successfully!!! V1.2.6";
+		return "v1.2.7";
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
