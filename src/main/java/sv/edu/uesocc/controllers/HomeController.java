a44cfc0f303c0cdeb85f4325c888bf7c79302ba4
Melbin <melbin.cruz40@gmail.com>
2020-08-11 11:39:40 -0600

    Changing hello-world version 1.2.15

--- src/main/java/sv/edu/uesocc/controllers/HomeController.java
+++ src/main/java/sv/edu/uesocc/controllers/HomeController.java
@@ -48,7 +48,7 @@ public class HomeController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
 		logger.info("Test Method was called");
-		return "v1.2.14";
+		return "v1.2.15";
 	}
 	
 	@RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
